5c3b89dd7e6bd9db573feacc59048d17c95aa9f2
Prince <p2577430@gmail.com>
2025-08-14 00:24:16 +0530

    did a hard question of pattern printing

--- loops/Pattern/Hard-que.cpp
+++ loops/Pattern/Hard-que.cpp
@@ -1,27 +1,66 @@
+// Enter the number of rows: 5
+// 1234554321
+// 1234**4321
+// 123****321
+// 12******21
+// 1********1
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n,nst,i,j;
+//     cout<<"Enter the number of rows: ";
+//     cin>>n;
+//     nst=0;
+//     for(i=0; i<n; i++)
+//     {
+//         for(j=1; j<=n-i; j++)
+//         {
+//             cout<<j;
+//         }
+//         for(j=1; j<=nst; j++)
+//         {
+//             cout<<"*";
+//         }
+//         for(j=n-i; j>=1; j--)
+//         {
+//             cout<<j;
+//         }
+//         cout<<endl;
+//         nst = nst + 2;
+
+//     }
+// }
+
 #include<iostream>
 using namespace std;
-int main()
-{
-    int n,nst,i,j;
+int main(){
+    int n;
     cout<<"Enter the number of rows: ";
     cin>>n;
-    nst=0;
-    for(i=0; i<n; i++)
-    {
-        for(j=1; j<=n-i; j++)
-        {
-            cout<<j;
+
+    int row = 1;
+    int nst = 1;
+    while(row <= n){
+
+        int col1 = 1;
+        int col2 = 1;
+        int col3 = n - row + 1;
+        while(col1 <= (n-row+1)){
+            cout<<col1;
+            col1++;
         }
-        for(j=1; j<=nst; j++)
-        {
+        while(col2 < nst){
             cout<<"*";
+            col2++;
         }
-        for(j=n-i; j>=1; j--)
-        {
-            cout<<j;
+        nst += 2;
+        while(col3){
+            cout<<col3;
+            col3--;
         }
         cout<<endl;
-        nst = nst + 2;
-
+        row++;
     }
 }
