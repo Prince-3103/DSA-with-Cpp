10abc810adffd5d45f428f3e46b486ecd242448c
Prince <p2577430@gmail.com>
2025-08-28 01:18:14 +0530

    creating bubble sort function

--- Array/sorting/bubble-sort.cpp
+++ Array/sorting/bubble-sort.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
+
+void bubbleSort(int arr[], int n){
+    for(int i = 0; i<n-1; i++){
+        for(int j = 0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                
+            }
+        }
+    }
+}
+
 int main(){
     int n;
     cout<<"Enter the size of array: ";
@@ -10,9 +22,10 @@ int main(){
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
+
     cout<<"Unsorted array: ";
     for(int i = 0; i<n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-}
+}   
