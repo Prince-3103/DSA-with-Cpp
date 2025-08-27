bce686d9c209ed6d303aad979772e6f990b1696f
Prince <p2577430@gmail.com>
2025-08-28 00:23:44 +0530

    creating another loop nested with minIndex loop in which compare the which value is smallest if the smallest value lowest than minIndex value change the minIndex to present value

--- Array/sorting/selection-sort.cpp
+++ Array/sorting/selection-sort.cpp
@@ -4,7 +4,11 @@ using namespace std;
 void selectionSort(int arr[], int size){
     for(int i = 0; i < (size-1); i++){
         int minIndex = i;
-        
+        for(int j = i+1; j < size; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
     }
 }
 
