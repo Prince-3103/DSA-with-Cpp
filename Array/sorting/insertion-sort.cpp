1fdea4667326e297184a57fbbbad8054decede54
Prince <p2577430@gmail.com>
2025-08-29 01:46:12 +0530

    Adding insertion sort

---
+++ Array/sorting/insertion-sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size){
+    
+    
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the element of array: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Unsorted array: ";
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
