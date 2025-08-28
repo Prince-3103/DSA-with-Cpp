749daeba446340c494c92bdc5d95916ded51f64f
Prince <p2577430@gmail.com>
2025-08-29 01:51:21 +0530

    Adding algorithm in insertion sort function

--- Array/sorting/insertion-sort.cpp
+++ Array/sorting/insertion-sort.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 void insertionSort(int arr[], int size){
     
-    
+    for(int i = 1; i < size; i++){
+        int temp = arr[i];
+        int j = i - 1;
+        while(j >= 0){
+            if(arr[j] > temp){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+            j--;
+        }
+        arr[j+1] = temp;
+    }
 }
 
 int main(){
