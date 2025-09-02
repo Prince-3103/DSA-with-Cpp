f6b55ab5bcd6f23d6999d86c091acb0d023e73fd
Prince <p2577430@gmail.com>
2025-09-02 23:27:20 +0530

    Adding a function to ignore if the user input special character rather than alpha numeric value

--- Array/string/palindrome.cpp
+++ Array/string/palindrome.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+bool isValid(char check){
+    
+    if((check>='0' && check<='9') || (check>='a' && check<='z')){
+        cout<<"Check ";
+        return true;
+    }
+    cout<<"ignore";
+    return false;
+    
+}
+
 void lowerCase(char s[], int len){
 
     int i = 0;
@@ -16,13 +27,25 @@ bool isPalindrome(char s[], int len){
 
     int start = 0;
     int end = len - 1;
-
+    
     while(start <= end){
-        if(s[start] != s[end]){
-            return false;
+        if(isValid(s[start]) && isValid(s[end])){
+            if(s[start] != s[end]){
+                cout<<"Wrong "<<s[start]<<" = "<<s[end]<<endl;
+                return false;
+            }
+            cout<<"Right "<<s[start]<<" = "<<s[end]<<endl;
+            start++;
+            end--;
         }
-        start++;
-        end--;
+        else if(!isValid(s[start])){
+            cout<<s[start]<<endl;
+            start++;
+        }
+        else{
+            cout<<s[end]<<endl;
+            end--;
+        }
     }
     return true;
 }
