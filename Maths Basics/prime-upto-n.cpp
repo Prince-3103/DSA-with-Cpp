32c1b041d6f512aac5145423af92bfad9bd20b60
Prince <p2577430@gmail.com>
2025-09-09 23:10:03 +0530

    starting loop from 2 and using conditional loop to check is number true and 2 is true so the count will increament

--- Maths Basics/prime-upto-n.cpp
+++ Maths Basics/prime-upto-n.cpp
@@ -11,6 +11,12 @@ int countPrime(int n){
     vector<bool> prime(n+1, true);
 
     prime[0] = prime[1] = false;
+
+    for(int i = 2; i<n; i++){
+        if(prime[i]){
+            count++;
+        }
+    }
 }
 
 int main(){
