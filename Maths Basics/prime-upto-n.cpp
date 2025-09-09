d75730a9cde2eaa1c61eda51d4d8568d1d08dfcb
Prince <p2577430@gmail.com>
2025-09-09 23:05:59 +0530

    Adding function to count prime number and creating a boolean array and all elements will be true

---
+++ Maths Basics/prime-upto-n.cpp
@@ -0,0 +1,19 @@
+// We use Sieve of Eratosthenes
+// Page number -> 43
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int countPrime(int n){
+    int count = 0;
+    vector<bool> prime(n+1, true);
+}
+
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+
+}
