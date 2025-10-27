2aae424668c2d369a226ae9c8f8ee7353bb13139
Prince <p2577430@gmail.com>
2025-10-27 19:50:50 +0530

    Learning hashing

---
+++ Hashing/hashing-1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the element of array: ";
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int hash[24] = {0};
+    for(int i = 0; i<n; i++){
+        hash[arr[i]] += 1;
+    }
+
+    int q; 
+    cout << "How many numbers you want to find: ";
+    cin >> q;
+    while(q--){
+        int num;
+        cout << "Enter number you want to find: ";
+        cin >> num;
+
+        // Fetch
+        cout << "Number of times " << num <<" appeared: " << hash[num] << endl;
+    }
+}
