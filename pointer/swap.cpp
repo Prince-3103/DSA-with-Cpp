7b4f95cf4449955d45d50ffb52c3ee5a5df825cd
Prince <p2577430@gmail.com>
2025-11-15 23:58:52 +0530

    Learning difference between pass by pointer or pass by reference

--- pointer/swap.cpp
+++ pointer/swap.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
 using namespace std;
 
-void swap(int *p1, int *p2){
-    int temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
+
+// Pass by Pointer
+// void swap(int *p1, int *p2){
+//     int temp = *p1;
+//     *p1 = *p2;
+//     *p2 = temp;
+// }
+
+// Pass by Reference
+void swap(int &p1, int &p2){
+    int temp = p1;
+    p1 = p2;
+    p2 = temp;
 }
 
 int main(){
     int a = 2, b = 4;
     cout << "num1= "<< a << ", num2= " << b << endl;
-    swap(&a, &b);
+    // swap(&a, &b);  // Pass by Pointer
+    swap(a,b);       // Pass by Reference
     cout << "num1= "<< a << ", num2= " << b << endl;
 }
